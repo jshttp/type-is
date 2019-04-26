5b04b8c035e3441c6674326605df118b98f86bec
Douglas Christopher Wilson <doug@somethingdoug.com>
2019-04-25 22:48:31 -0400

    Use content-type and media-typer for type validation
    closes #39

--- index.js
+++ index.js
@@ -12,8 +12,9 @@
  * @private
  */
 
-var typer = require('content-type')
+var contentType = require('content-type')
 var mime = require('mime-types')
+var typer = require('media-typer')
 
 /**
  * Module exports.
@@ -237,13 +238,13 @@ function mimeMatch (expected, actual) {
 
 function normalizeType (value) {
   // parse the type
-  var type = typer.parse(value)
+  var type = contentType.parse(value).type
 
-  // remove the parameters
-  type.parameters = undefined
+  if (!typer.test(type)) {
+    return null
+  }
 
-  // reformat it
-  return typer.format(type)
+  return type
 }
 
 /**
